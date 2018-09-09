32ef6b7e62ad61a4126f3c54aa70a438b0538a22
Matthew Grogan <matt.grogan@gia.edu>
2018-09-09 16:10:44 -0400

    Fixed for rpi

---
+++ info_screen/rpi/rpi.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import
+
+import time
+import pygame
+from pygame.locals import *
+
+from messaging.command_connection import CommandConnection
+from messaging.image_connection import ImageConnection
+
+MATRIX_WIDTH = 800
+MATRIX_HEIGHT = 480
+
+CMD_HOST = "localhost"
+CMD_PORT = 5555
+
+IMG_HOST = "localhost"
+IMG_PORT = 5556
+
+FPS = 30
+
+class Rpi(object):
+
+    def __init__(self):
+        # Connect to receive images
+        self.image_conn = ImageConnection()
+        self.image_conn.connect(IMG_HOST, IMG_PORT, as_receiver=True)
+
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((MATRIX_WIDTH, MATRIX_HEIGHT))
+        pygame.display.set_caption("Info Screen")
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+
+    def mainloop(self):
+
+        while self.running:
+            self.clock.tick(FPS)
+
+            for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
+                ## listening for the the X button at the top
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            im = self.image_conn.receive()
+            if im is not None:
+                self.display(im)
+
+    def display(self, im):
+        
+        raw_str = im.tostring()
+        s = pygame.image.fromstring(raw_str, (MATRIX_WIDTH, MATRIX_HEIGHT), 'RGB')
+
+        
+        self._display_surf.blit(s, (0, 0))
+        pygame.display.flip()
+            
+
+            
+        
+
+
