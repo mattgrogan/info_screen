bf35873ec9d34aba627275440750d3d509f495b7
Matthew Grogan <matt.grogan@gia.edu>
2019-12-07 08:13:47 -0500

    added mta

--- info_screen/server/components/screens/clock_screen.py
+++ info_screen/server/components/screens/clock_screen.py
@@ -9,7 +9,7 @@ from server.components.layers.scrolling_layer import ScrollingLayer
 from server.components.screens.gif_screen_factory import GifScreenFactory
 
 from server.data.current_conditions import NOAA_Current_Observation, IconDecoder
-from server.data.showerthoughts import ShowerThoughts
+from server.data.mta_status import MTA_Status
 
 STATION = "KFRG"
 ST_INTERVAL = 60 * 10 # every 10 minutes
@@ -24,17 +24,18 @@ class ClockScreen(Screen):
         # Create a connection to the NOAA website 
         self.current_obs = NOAA_Current_Observation(STATION)
 
-        # Create a showerthoughts connection
-        self.showerthoughts = ShowerThoughts()
-        self.last_showerthoughts = None
+        # Create a connection to MTA
+        self.service_status = MTA_Status()
+        self.line = "Port Jefferson"
 
         # Load the font
-        self.vl_font = FontFactory().by_name( "enhanced_led_board-7", 120)
-        self.lg_font = FontFactory().by_name( "enhanced_led_board-7", 80)
-        self.md_font = FontFactory().by_name( "enhanced_led_board-7", 48)
-        self.sm_font = FontFactory().by_name( "enhanced_led_board-7", 24)
-        self.vs_font = FontFactory().by_name( "enhanced_led_board-7", 16)
-        self.ty_font = FontFactory().by_name( "enhanced_led_board-7", 8)
+        self.el_font = FontFactory().by_name( "OpenSans-Regular", 160)
+        self.vl_font = FontFactory().by_name( "OpenSans-Regular", 120)
+        self.lg_font = FontFactory().by_name( "OpenSans-Regular", 80)
+        self.md_font = FontFactory().by_name( "OpenSans-Regular", 48)
+        self.sm_font = FontFactory().by_name( "OpenSans-Regular", 24)
+        self.vs_font = FontFactory().by_name( "OpenSans-Regular", 16)
+        self.ty_font = FontFactory().by_name( "OpenSans-Regular", 8)
 
         self.lg_icons = FontFactory().by_name( "weathericons-regular-webfont", 120)
         self.md_icons = FontFactory().by_name( "weathericons-regular-webfont", 32)
@@ -51,20 +52,23 @@ class ClockScreen(Screen):
 
         # Time and Date
 
-        self.time_text = TextLayer(self.vl_font, self.time_color, lambda: time.strftime("%I:%M", time.localtime()).lstrip("0"))
+        self.date_text = TextLayer(self.md_font, self.time_color, lambda: time.strftime("%A, %B ") + time.strftime("%d").lstrip("0"))
+        self.date_text.padding = (10, 5, 0, 0)
+        
+        self.time_text = TextLayer(self.el_font, self.time_color, lambda: time.strftime("%I:%M", time.localtime()).lstrip("0"))
         self.time_text.padding = (10, 10, 0, 0)
+        self.time_text.top_items = [self.date_text]
 
         self.am_pm = TextLayer(self.md_font, self.time_color, lambda: time.strftime("%p", time.localtime()))
         self.am_pm.padding = (10, 10, 0, 0)
+        self.am_pm.top_items = [self.date_text]
+
         self.am_pm.left_items = [self.time_text]
 
-        self.date_text = TextLayer(self.vs_font, self.time_color, lambda: time.strftime("%A %B ") + time.strftime("%d").lstrip("0"))
-        self.date_text.padding = (10, 5, 0, 0)
-        self.date_text.top_items = [self.time_text]
 
         # Outside conditions
 
-        self.outside_conditions = TextLayer(self.vs_font, self.outside_color, lambda: "OUTSIDE CONDITIONS")
+        self.outside_conditions = TextLayer(self.sm_font, self.outside_color, lambda: "Current Conditions")
         self.outside_conditions.padding = (10, 20, 0, 0)
         self.outside_conditions.top_items = [self.time_text, self.date_text]
 
@@ -73,39 +77,35 @@ class ClockScreen(Screen):
         self.weather_icon.top_items = [self.time_text, self.date_text, self.outside_conditions]
 
         self.temp = TextLayer(self.md_font, self.outside_color, self._temp)
-        self.temp.padding = (10, 30, 0, 0)
+        self.temp.padding = (10, 15, 0, 0)
         self.temp.top_items = [self.time_text, self.date_text, self.outside_conditions]
         self.temp.left_items = [self.weather_icon]
 
         self.temp_icon = TextLayer(self.md_icons, self.outside_color, lambda: u"\uF045")
-        self.temp_icon.padding = (5, 30, 0, 0)
+        self.temp_icon.padding = (5, 15, 0, 0)
         self.temp_icon.top_items = [self.time_text, self.date_text, self.outside_conditions]
         self.temp_icon.left_items = [self.weather_icon, self.temp]
 
         self.rh = TextLayer(self.md_font, self.outside_color, self._rh)
-        self.rh.padding = (10, 30, 0, 0)
-        self.rh.top_items =[self.time_text, self.date_text, self.outside_conditions]
-        self.rh.left_items = [self.weather_icon, self.temp, self.temp_icon]
+        self.rh.padding = (10, 15, 0, 0)
+        self.rh.top_items =[self.time_text, self.date_text, self.outside_conditions, self.temp]
+        self.rh.left_items = [self.weather_icon]
 
         self.rh_icon = TextLayer(self.sm_icons, self.outside_color, lambda: u"\uF07A")
-        self.rh_icon.padding = (5, 30, 0, 0)
-        self.rh_icon.top_items = [self.time_text, self.date_text, self.outside_conditions]
-        self.rh_icon.left_items = [self.weather_icon, self.temp, self.temp_icon, self.rh]
+        self.rh_icon.padding = (5, 15, 0, 0)
+        self.rh_icon.top_items = [self.time_text, self.date_text, self.outside_conditions, self.temp]
+        self.rh_icon.left_items = [self.weather_icon, self.rh]
 
         # Weather text
 
-        self.weather = TextLayer(self.vs_font, self.outside_color, self._weather)
+        self.weather = TextLayer(self.sm_font, self.outside_color, self._weather)
         self.weather.padding = (10, 5, 0, 0)
-        self.weather.top_items = [self.time_text, self.date_text, self.weather_icon, self.outside_conditions]
+        self.weather.top_items = [self.time_text, self.date_text, self.outside_conditions, self.weather_icon]
 
-        # Showerthoughts
-
-        self.st = TextLayer(self.ty_font, self.st_color, self.showerthoughts.random)
-        self.st.padding = (10, 20, 0, 0)
-        self.st.top_items = self.weather.top_items + [self.weather]
-
-        self.st.step()
-        self.last_showerthoughts = time.time()
+        # MTA Status
+        self.port_jeff = TextLayer(self.sm_font, "#0039a6", self._service_status)
+        self.port_jeff.padding = (5, 15, 0, 0)
+        self.port_jeff.top_items = [self.time_text, self.date_text, self.outside_conditions, self.weather_icon, self.weather]
 
     def enter(self):
         pass
@@ -131,10 +131,7 @@ class ClockScreen(Screen):
         self.rh.step()
         self.rh_icon.step()
         self.weather.step()
-
-        if time.time() > self.last_showerthoughts + ST_INTERVAL:
-            self.st.step()
-            self.last_showerthoughts = time.time()
+        self.port_jeff.step()
 
     def _temp(self):
         try:
@@ -168,7 +165,15 @@ class ClockScreen(Screen):
 
         icon = IconDecoder()
 
-        return icon.lookup(w_str)               
+        return icon.lookup(w_str)
+
+    def _service_status(self):
+        try:
+            s_str = self.line + ": " + self.service_status[self.line]
+        except ValueError:
+            return "NA"
+
+        return s_str          
 
     def render(self):
         
@@ -186,8 +191,7 @@ class ClockScreen(Screen):
         self.rh.render(bg)
         self.rh_icon.render(bg)
         self.weather.render(bg)
-
-        self.st.render(bg)
+        self.port_jeff.render(bg)
 
         return bg
 
---
+++ info_screen/server/data/mta_status.py
@@ -0,0 +1,72 @@
+from __future__ import absolute_import
+
+import os
+import requests
+import xml.etree.ElementTree as ET
+
+from server.data.throttle_mixin import Throttle_Mixin
+
+UPDATE_FREQUENCY = 60 * 10 # Every ten minutes
+SERVICE_STATUS_URL = "http://web.mta.info/status/serviceStatus.txt"
+
+class MTA_Status(Throttle_Mixin):
+  """
+  This class handles connections to mta.info for the current service status.
+  """
+
+  def __init__(self):
+
+    self.url = SERVICE_STATUS_URL
+
+    self.service_status = {}   # Dict to hold data
+
+    # Use the Mixin to throttle requests
+    self.every(UPDATE_FREQUENCY, self._fetch_data)
+
+  def __getitem__(self, name):
+    """
+    Checks for new data and returns the appropriate value.
+    """
+
+    # Call mixin
+    self.run_pending()
+
+    if name in self.service_status.keys():
+      return self.service_status[name]
+    else:
+      raise ValueError("No value for '%s'" % name)
+
+  def _fetch_data(self):
+
+    # Attempt a connection
+    try:
+      r = requests.get(self.url, timeout=10)
+    except:
+      return False
+
+    if r.status_code == requests.codes.ok:
+      # Parse the data
+      try:
+        root = ET.fromstring(r.content)
+      except:
+        return False
+        
+      for child in root:
+        if child.tag == "LIRR":
+              for grandchild in child:
+                line = grandchild.find("name").text
+                status = grandchild.find("status").text
+                self.service_status[line] = status
+      return True
+    else:
+      return False
+
+if __name__ == "__main__":
+
+  cc = MTA_Status()
+
+  print cc["Port Jefferson"]
+
+  for key in cc.service_status.keys():
+    print "%s:   %s" % (key, cc[key])
+  
