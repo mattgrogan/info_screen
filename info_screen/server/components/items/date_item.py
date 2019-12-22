a75de7e77aa501b8d8680c439977abdc8a3a81c3
Matthew Grogan <matt.grogan@gia.edu>
2019-12-22 16:50:06 -0500

    lint

--- info_screen/server/components/items/date_item.py
+++ info_screen/server/components/items/date_item.py
@@ -1,11 +1,24 @@
+"""Shows the date in Day, Month DD format
+"""
 import time
 
 from server.components.layers.text_layer import TextLayer
 from util.font_factory import FontFactory
 
 class DateItem(TextLayer):
+    """Date in Day, Month DD format
+    
+    :param TextLayer: [description]
+    :type TextLayer: [type]
+    
+    """
     def __init__(self):
         self.font = FontFactory().by_name( "OpenSans-Regular", 48)
         self.color = "#FFFFFF"
         self.callback = lambda: time.strftime("%A, %B ") + time.strftime("%d").lstrip("0")
         super(DateItem, self).__init__(self.font, self.color, self.callback)
+
+
+
+
+
